06914dbe69635f7d8192f815cc1d49009c03c6b0
CesarLopezz <cesar.lopez@correo.unimet.edu.ve>
2023-10-07 01:53:17 -0400

    menu listo

--- src/Interfaces/CargarArchivo.java
+++ src/Interfaces/CargarArchivo.java
@@ -8,14 +8,21 @@ package Interfaces;
  *
  * @author cesar
  */
-public class Menu extends javax.swing.JFrame {
+public class CargarArchivo extends javax.swing.JFrame {
+    public static Menu v1;
 
     /**
-     * Creates new form Menu
+     * Creates new form CargaArchivo
      */
-    public Menu() {
+    public CargarArchivo(Menu v1) {
         initComponents();
+        this.v1=v1;
+        v1.setVisible(false);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        
     }
+   
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -27,45 +34,19 @@ public class Menu extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        CargarArchivo = new javax.swing.JButton();
-        ModificarGrafo = new javax.swing.JButton();
-        CompsFuerteConex = new javax.swing.JButton();
-        ActuRepo1 = new javax.swing.JButton();
-        MostrarGrafo1 = new javax.swing.JButton();
-        EXIT = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jFileChooser1 = new javax.swing.JFileChooser();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 204));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        CargarArchivo.setText("CARGAR ARCHIVO");
-        CargarArchivo.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                CargarArchivoActionPerformed(evt);
-            }
-        });
-        jPanel1.add(CargarArchivo, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 170, 320, -1));
-
-        ModificarGrafo.setText("MODIFICAR GRAFO");
-        jPanel1.add(ModificarGrafo, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 200, 320, -1));
-
-        CompsFuerteConex.setText("COMPONENTES FUERTEMENTE CONECTADOS");
-        jPanel1.add(CompsFuerteConex, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 290, 320, -1));
-
-        ActuRepo1.setText("ACTUALIZAR REPOSITORIO");
-        jPanel1.add(ActuRepo1, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 230, 320, -1));
-
-        MostrarGrafo1.setText("MOSTRAR GRAFO");
-        jPanel1.add(MostrarGrafo1, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 260, 320, -1));
-
-        EXIT.setText("X");
-        EXIT.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                EXITActionPerformed(evt);
-            }
-        });
-        jPanel1.add(EXIT, new org.netbeans.lib.awtextra.AbsoluteConstraints(720, 460, 70, -1));
+        jLabel1.setBackground(new java.awt.Color(0, 0, 0));
+        jLabel1.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
+        jLabel1.setText("Ingrese el archivo TXT:");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, 230, -1));
+        jPanel1.add(jFileChooser1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 100, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -80,15 +61,6 @@ public class Menu extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void EXITActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_EXITActionPerformed
-        this.dispose();
-    }//GEN-LAST:event_EXITActionPerformed
-
-    private void CargarArchivoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CargarArchivoActionPerformed
-        CargarArchivo ventana = new CargarArchivo(this);
-        
-    }//GEN-LAST:event_CargarArchivoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -107,31 +79,28 @@ public class Menu extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CargarArchivo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CargarArchivo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CargarArchivo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CargarArchivo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Menu().setVisible(true);
+                new CargarArchivo(v1).setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton ActuRepo1;
-    private javax.swing.JButton CargarArchivo;
-    private javax.swing.JButton CompsFuerteConex;
-    private javax.swing.JButton EXIT;
-    private javax.swing.JButton ModificarGrafo;
-    private javax.swing.JButton MostrarGrafo1;
+    private javax.swing.JFileChooser jFileChooser1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
