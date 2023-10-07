ff3b00275bea8873dc946830546674ca91c476fc
luisfdasilva <113453770+luisfdasilva@users.noreply.github.com>
2023-10-07 18:28:38 -0400

    cambio del codigo lectura y guardado del txt de main a cargarArchivo y arreglo en el codigo

--- src/Interfaces/CargaArchivo.java
+++ src/Interfaces/CargaArchivo.java
@@ -3,25 +3,98 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package Interfaces;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JFileChooser;
 /**
  *
  * @author cesar
  */
-public class CargaArchivo extends javax.swing.JFrame {
-    
-    public static Menu v1;
+/**este codigo importando JFilechooser lee el archio txt y los almacenas de dos arrayys de String, una para usuarios y otra para relaciones.Finalmene, imprime el codigo en la consola.
+ * @author luis
+ * @version:07/10/23
+ */
 
-    /**
-     * Creates new form CargaArchivo
-     */
-    public CargaArchivo(Menu v1) {
-        initComponents();
-        this.v1=v1;
-        v1.setVisible(false);
-        this.setVisible(true);
-        this.setLocationRelativeTo(null);
-    }
+public class CargaArchivo extends javax.swing.JFrame {
+        public static void main(String[] args) throws IOException {
+
+            // Creamos el JFileChooser
+            JFileChooser jfc = new JFileChooser();
+
+            // Abrimos el cuadro de diálogo de selección de archivos
+            int seleccion = jfc.showOpenDialog(null);
+
+            // Si el usuario seleccionó un archivo
+            if (seleccion == JFileChooser.APPROVE_OPTION) {
+
+                // Obtenemos el archivo seleccionado
+                File archivo = jfc.getSelectedFile();
+
+                // Creamos un flujo de entrada para leer el archivo
+                FileReader fr = new FileReader(archivo);
+
+                // Declaramos dos arrays para almacenar la información
+                String[] usuarios = new String[100];
+                String[] relaciones = new String[100];
+
+                int i = 0;
+                int j = 0;
+
+                // Leemos los datos del archivo
+                BufferedReader br = new BufferedReader(fr);
+                String linea;
+                while ((linea = br.readLine()) != null) {
+
+                    // Si la línea es un usuario
+                    if (linea.startsWith("Usuario")) {
+
+                        // Agregamos la línea al array de usuarios
+                        usuarios[i] = linea;
+                        i++;
+
+                    } else {
+
+                        // Agregamos la línea al array de relaciones
+                        relaciones[j] = linea;
+                        j++;
+                    }
+                }
+
+                // Cerramos el flujo de entrada
+                br.close();
+
+                // Imprimimos la información de los usuarios
+                System.out.println("Usuarios:");
+                for (String usuario : usuarios) {
+                    if (usuario != null) {
+                        System.out.println(usuario);
+                    }
+                }
+
+                // Imprimimos la información de las relaciones
+                System.out.println("Relaciones:");
+                for (String relacion : relaciones) {
+                    if (relacion != null) {
+                        System.out.println(relacion);
+                    }
+                }
+            }
+        }    
+    
+        public static Menu v1;
+
+        /**
+         * Creates new form CargaArchivo
+         */
+        public CargaArchivo(Menu v1) {
+            initComponents();
+            this.v1=v1;
+            v1.setVisible(false);
+            this.setVisible(true);
+            this.setLocationRelativeTo(null);
+        }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -62,37 +135,38 @@ public class CargaArchivo extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
+        public static void main(String args[]) {
+            /* Set the Nimbus look and feel */
+            //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+            /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+             * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+             */
+            try {
+                for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                    if ("Nimbus".equals(info.getName())) {
+                        javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                        break;
+                    }
                 }
+            } catch (ClassNotFoundException ex) {
+                java.util.logging.Logger.getLogger(CargaArchivo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (InstantiationException ex) {
+                java.util.logging.Logger.getLogger(CargaArchivo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+                java.util.logging.Logger.getLogger(CargaArchivo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+                java.util.logging.Logger.getLogger(CargaArchivo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CargaArchivo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CargaArchivo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CargaArchivo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CargaArchivo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+            //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new CargaArchivo(v1).setVisible(true);
-            }
-        });
-    }
+            /* Create and display the form */
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    new CargaArchivo(v1).setVisible(true);
+                }
+            });
+
+        }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
