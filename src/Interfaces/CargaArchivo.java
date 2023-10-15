ed7684075f0093ed5bbffd7046a4fa80d484a8ea
luisfdasilva <113453770+luisfdasilva@users.noreply.github.com>
2023-10-15 12:14:06 -0400

    lectura del txt al main

--- src/Interfaces/CargaArchivo.java
+++ src/Interfaces/CargaArchivo.java
@@ -3,11 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package Interfaces;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import javax.swing.JFileChooser;
+//import java.io.BufferedReader;
+//import java.io.File;
+//import java.io.FileReader;
+//import java.io.IOException;
+//import javax.swing.JFileChooser;
 /**
  *
  * @author cesar
@@ -18,70 +18,70 @@ import javax.swing.JFileChooser;
  */
 
 public class CargaArchivo extends javax.swing.JFrame {
-        public static void main(String[] args) throws IOException {
-
-            // Creamos el JFileChooser
-            JFileChooser jfc = new JFileChooser();
-
-            // Abrimos el cuadro de diálogo de selección de archivos
-            int seleccion = jfc.showOpenDialog(null);
-
-            // Si el usuario seleccionó un archivo
-            if (seleccion == JFileChooser.APPROVE_OPTION) {
-
-                // Obtenemos el archivo seleccionado
-                File archivo = jfc.getSelectedFile();
-
-                // Creamos un flujo de entrada para leer el archivo
-                FileReader fr = new FileReader(archivo);
-
-                // Declaramos dos arrays para almacenar la información
-                String[] usuarios = new String[100];
-                String[] relaciones = new String[100];
-
-                int i = 0;
-                int j = 0;
-
-                // Leemos los datos del archivo
-                BufferedReader br = new BufferedReader(fr);
-                String linea;
-                while ((linea = br.readLine()) != null) {
-
-                    // Si la línea es un usuario
-                    if (linea.startsWith("Usuario")) {
-
-                        // Agregamos la línea al array de usuarios
-                        usuarios[i] = linea;
-                        i++;
-
-                    } else {
-
-                        // Agregamos la línea al array de relaciones
-                        relaciones[j] = linea;
-                        j++;
-                    }
-                }
-
-                // Cerramos el flujo de entrada
-                br.close();
-
-                // Imprimimos la información de los usuarios
-                System.out.println("Usuarios:");
-                for (String usuario : usuarios) {
-                    if (usuario != null) {
-                        System.out.println(usuario);
-                    }
-                }
-
-                // Imprimimos la información de las relaciones
-                System.out.println("Relaciones:");
-                for (String relacion : relaciones) {
-                    if (relacion != null) {
-                        System.out.println(relacion);
-                    }
-                }
-            }
-        }    
+//        public static void main(String[] args) throws IOException {
+//
+//            // Creamos el JFileChooser
+//            JFileChooser jfc = new JFileChooser();
+//
+//            // Abrimos el cuadro de diálogo de selección de archivos
+//            int seleccion = jfc.showOpenDialog(null);
+//
+//            // Si el usuario seleccionó un archivo
+//            if (seleccion == JFileChooser.APPROVE_OPTION) {
+//
+//                // Obtenemos el archivo seleccionado
+//                File archivo = jfc.getSelectedFile();
+//
+//                // Creamos un flujo de entrada para leer el archivo
+//                FileReader fr = new FileReader(archivo);
+//
+//                // Declaramos dos arrays para almacenar la información
+//                String[] usuarios = new String[100];
+//                String[] relaciones = new String[100];
+//
+//                int i = 0;
+//                int j = 0;
+//
+//                // Leemos los datos del archivo
+//                BufferedReader br = new BufferedReader(fr);
+//                String linea;
+//                while ((linea = br.readLine()) != null) {
+//
+//                    // Si la línea es un usuario
+//                    if (linea.startsWith("Usuario")) {
+//
+//                        // Agregamos la línea al array de usuarios
+//                        usuarios[i] = linea;
+//                        i++;
+//
+//                    } else {
+//
+//                        // Agregamos la línea al array de relaciones
+//                        relaciones[j] = linea;
+//                        j++;
+//                    }
+//                }
+//
+//                // Cerramos el flujo de entrada
+//                br.close();
+//
+//                // Imprimimos la información de los usuarios
+//                System.out.println("Usuarios:");
+//                for (String usuario : usuarios) {
+//                    if (usuario != null) {
+//                        System.out.println(usuario);
+//                    }
+//                }
+//
+//                // Imprimimos la información de las relaciones
+//                System.out.println("Relaciones:");
+//                for (String relacion : relaciones) {
+//                    if (relacion != null) {
+//                        System.out.println(relacion);
+//                    }
+//                }
+//            }
+//        }    
     
         public static Menu v1;
 
